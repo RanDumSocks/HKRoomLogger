9600db3ec9299ac14e9cc979a835b69a01ce0638
RanDumSocks <justinchance@randumsocks.com>
2023-03-07 13:47:20 -0800

    added logging

--- HKRoomLogger/HKRoomLogger.cs
+++ HKRoomLogger/HKRoomLogger.cs
@@ -1,10 +1,22 @@
-using Modding;
 using System;
+using System.IO;
+using Modding;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace HKRoomLogger
 {
     public class HKRoomLoggerMod : Mod
     {
+        public static string LogPath => Path.Combine(Application.persistentDataPath, "RoomLogger.json");
+
+        public struct OutData
+        {
+            public string last;
+            public string current;
+        }
+
         private static HKRoomLoggerMod? _instance;
 
         internal static HKRoomLoggerMod Instance
@@ -30,9 +42,30 @@ namespace HKRoomLogger
         {
             Log("Initializing");
 
-            // put additional initialization logic here
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChange;
 
             Log("Initialized");
         }
+        public void OnSceneChange(Scene oldScene, Scene newScene)
+        {
+            var data = new OutData
+            {
+                last = oldScene.name,
+                current = newScene.name
+            };
+
+            Log($"{data.last} --> {data.current}");
+
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(LogPath));
+                File.WriteAllText(LogPath, JsonConvert.SerializeObject(data));
+            }
+            catch (Exception e)
+            {
+                LogError($"Error printing log to {LogPath}:\n{e}");
+            }
+        }
+
     }
 }
